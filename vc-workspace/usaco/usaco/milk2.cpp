2310c19dea1c9d0fb726a6eee630fdce6f8afcbd
catena <vishnusuresh1729@gmail.com>
2014-09-04 01:04:10 +0530

    uhunt ch1 starred problems complete

--- vc-workspace/usaco/usaco/milk2.cpp
+++ vc-workspace/usaco/usaco/milk2.cpp
@@ -14,56 +14,56 @@ using namespace std;
 #define NODEBUG		// <change>
 
 struct interval {
-	int start, finish;
+    int start, finish;
 
-	interval() : start(0), finish(0) {}
+    interval() : start(0), finish(0) {}
 
-	friend istream& operator>>(istream& in, interval& t) {
-		in >> t.start >> t.finish;
-		return in;
-	}
+    friend istream& operator>>(istream& in, interval& t) {
+        in >> t.start >> t.finish;
+        return in;
+    }
 
-	bool operator<(const interval& other) const {
-		return start < other.start;
-	}
+    bool operator<(const interval& other) const {
+        return start < other.start;
+    }
 };
 
 int main() {
 
 #ifdef DEBUG
-	ifstream fin(stdin);
-	ofstream fout(stdout);	
+    ifstream fin(stdin);
+    ofstream fout(stdout);	
 #else
-	ifstream fin("milk2.in");	// <change>
-	ofstream fout("milk2.out");	// <change>
+    ifstream fin("milk2.in");	// <change>
+    ofstream fout("milk2.out");	// <change>
 #endif
-	vector<interval> time;
-	
-	int N;
-	fin >> N;	
-	for (int i = 0; i < N; i++) {
-		interval t;
-		fin >> t;
-		time.push_back(t);
-	}
+    vector<interval> time;
 
-	sort(time.begin(), time.end());
+    int N;
+    fin >> N;	
+    for (int i = 0; i < N; i++) {
+        interval t;
+        fin >> t;
+        time.push_back(t);
+    }
 
-	int working, idle;
-	interval curr = time[0];
-	working = time[0].finish - time[0].start;
-	idle = 0;
-	
-	for (int i = 1; i < N; i++) {
-		if (time[i].start > curr.finish) {
-			curr.start = time[i].start;
-			idle = max(idle, time[i].start - curr.finish);
-		}
-		curr.finish = max(curr.finish, time[i].finish);
-		working = max(working, curr.finish - curr.start);
-	}
+    sort(time.begin(), time.end());
 
-	fout << working << " " << idle << "\n";
+    int working, idle;
+    interval curr = time[0];
+    working = time[0].finish - time[0].start;
+    idle = 0;
 
-	return EXIT_SUCCESS;
+    for (int i = 1; i < N; i++) {
+        if (time[i].start > curr.finish) {
+            curr.start = time[i].start;
+            idle = max(idle, time[i].start - curr.finish);
+        }
+        curr.finish = max(curr.finish, time[i].finish);
+        working = max(working, curr.finish - curr.start);
+    }
+
+    fout << working << " " << idle << "\n";
+
+    return EXIT_SUCCESS;
 }
